1bc8e0bee3c7d8e3cb9f7adc7bdc5c34fa70e8d2
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-16 10:47:46 +0300

    Добавьте файлы проекта.

---
+++ Lab23/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lab23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n=Convert.ToInt32(Console.ReadLine());
+            GetFactAsync(n);
+            Hello(n);//создала метод чтобы проверить асинхронность
+            Console.ReadKey();
+
+        }
+
+        static int GetFact (int n)
+        {
+            int nfact = 1;
+            for (int i = 2; i <= n; i++) 
+            {
+                nfact*= i;
+                Thread.Sleep(15);
+            }
+            Console.WriteLine("n!={0}",nfact);
+            return nfact;
+        }
+
+        static async Task<int> GetFactAsync(int n)
+        {
+            int result=await Task.Run(() => GetFact(n));
+            return result;
+        }
+
+        static void Hello(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Hello", i);
+            }
+        }
+    }
+        
+}
